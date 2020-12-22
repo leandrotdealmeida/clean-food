637d60f529f0f45921bd6af93c6cfd65a1324bd7
Leandro Almeida <leandrotrovilho@gmail.com>
2020-12-22 18:58:59 -0300

    Add project

---
+++ src/main/java/com/devjapa/cleanfood/CleanfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.devjapa.cleanfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjapa/cleanfood/CleanfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjapa.cleanfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
