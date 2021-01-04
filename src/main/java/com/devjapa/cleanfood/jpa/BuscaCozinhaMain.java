a0e07f733f223ba701db28fe3987a8b16af97e2e
Leandro Almeida <leandrotrovilho@gmail.com>
2021-01-04 18:48:27 -0200

    feat(JPA): Add main classes for test jpa

---
+++ src/main/java/com/devjapa/cleanfood/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.devjapa.cleanfood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.devjapa.cleanfood.CleanfoodApiApplication;
+import com.devjapa.cleanfood.domain.model.Cozinha;
+
+public class BuscaCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(CleanfoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		System.out.println(cozinha.getNome());
+	}
+
+}
---
+++ src/main/java/com/devjapa/cleanfood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.devjapa.cleanfood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.devjapa.cleanfood.CleanfoodApiApplication;
+import com.devjapa.cleanfood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(CleanfoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
+
+}
