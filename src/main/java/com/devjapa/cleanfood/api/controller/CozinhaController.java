16c902577a5b070d23accf2b3ec00e0ae49a587b
Leandro Almeida <leandrotrovilho@gmail.com>
2021-01-19 11:42:06 -0200

    feat(Cozinha Controller): Criando primeiro endpoint para cozinha

---
+++ src/main/java/com/devjapa/cleanfood/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.devjapa.cleanfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjapa.cleanfood.domain.model.Cozinha;
+import com.devjapa.cleanfood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
+	}
+
+}
