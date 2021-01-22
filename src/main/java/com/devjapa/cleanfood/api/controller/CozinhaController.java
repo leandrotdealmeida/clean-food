510e0344844e146c26961e1fcc935e44f41855d7
Leandro Almeida <leandrotrovilho@gmail.com>
2021-01-22 12:16:13 -0200

    feat(Xml Wrapper)

--- src/main/java/com/devjapa/cleanfood/api/controller/CozinhaController.java
+++ src/main/java/com/devjapa/cleanfood/api/controller/CozinhaController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devjapa.cleanfood.api.model.CozinhasXmlWrapper;
 import com.devjapa.cleanfood.domain.model.Cozinha;
 import com.devjapa.cleanfood.domain.repository.CozinhaRepository;
 
@@ -24,10 +26,20 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
+//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+//	public List<Cozinha> listar2() {
+//		System.out.println("Lista 2");
+//		return cozinhaRepository.listar();
+//	}	
+	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public List<Cozinha> listar2() {
-		System.out.println("Lista 2");
-		return cozinhaRepository.listar();
+	public CozinhasXmlWrapper listarXml() {		
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}	
+	
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable("id") Long id) {
+		return cozinhaRepository.buscar(id);
 	}
 
 }
---
+++ src/main/java/com/devjapa/cleanfood/api/model/CozinhasXmlWrapper.java
@@ -0,0 +1,22 @@
+package com.devjapa.cleanfood.api.model;
+
+import java.util.List;
+
+import com.devjapa.cleanfood.domain.model.Cozinha;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@JacksonXmlRootElement(localName = "cozinhas")
+@Data
+public class CozinhasXmlWrapper {
+	
+	@JsonProperty("cozinha")
+	@JacksonXmlElementWrapper(useWrapping = false)
+	@NonNull
+	List<Cozinha> cozinhas;
+
+}
