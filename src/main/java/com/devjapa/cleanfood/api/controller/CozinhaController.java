524637b5b31b5efefc06c394db644843799e3a01
Leandro Almeida <leandrotrovilho@gmail.com>
2021-02-25 16:39:50 -0300

    feat(Status): ResponseStatus

--- src/main/java/com/devjapa/cleanfood/api/controller/CozinhaController.java
+++ src/main/java/com/devjapa/cleanfood/api/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package com.devjapa.cleanfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devjapa.cleanfood.api.model.CozinhasXmlWrapper;
@@ -37,9 +39,10 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}	
 	
-	@GetMapping("/{id}")
-	public Cozinha buscar(@PathVariable("id") Long id) {
-		return cozinhaRepository.buscar(id);
+	//@ResponseStatus(value = HttpStatus.CREATED)
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable("cozinhaId") Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
 	}
 
 }
